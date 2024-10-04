1b4a4453bccfa23549ac1c2e3daa4776a12e6383
sei-tspencer <trspencer@sei.cmu.edu>
2024-10-04 13:40:20 -0400

    added signalR stateful reconnect

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 // Copyright 2022 Carnegie Mellon University. All Rights Reserved.
 // Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
-
+declare var require: any;
 export const environment = {
   production: true,
   VERSION: require('../../package.json').version,
 };
-
