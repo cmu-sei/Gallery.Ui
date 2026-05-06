8108d39cfd4b6168c0fdfd9aae81ea6d4946c6aa
Adam Welle <arwelle@sei.cmu.edu>
2026-05-06 21:30:41 +0000

    Regenerate API client with openapi-generator v7.2.0
    Generated using cross-platform Docker-based swagger-gen-docker script

--- src/app/generated/api/index.ts
+++ src/app/generated/api/index.ts
@@ -1,11 +1,6 @@
-/*
- Copyright 2023 Carnegie Mellon University. All Rights Reserved.
- Released under a MIT (SEI)-style license. See LICENSE.md in the
- project root for license information.
-*/
-
 export * from './api/api';
 export * from './model/models';
 export * from './variables';
 export * from './configuration';
 export * from './api.module';
+export * from './param';
